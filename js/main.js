9704ddc5b7b3b9f05967871070cdee11f2e3b6fa
Antonia Schmalstieg <antonia.schmalstieg@codecentric.de>
2019-11-15 16:22:35 +0100

    Select random set of choices for bubbles and display message when all are solved

--- js/main.js
+++ js/main.js
@@ -20,10 +20,12 @@ class Choice {
 var animalImageElement = document.getElementById('animalImage');
 
 var choiceGreen = new Choice('./video/grün.mp4','grün',true);
-var choiceWollknaeul= new Choice('./video/wollknäuel.mp4','Wollknäuel',false);
-var choiceWasser= new Choice('./video/wasser.mp4','Wasser',true);
-var choiceSchaf= new Choice('./video/schaf.mp4','Schaf',false);
-var choiceHuepfen= new Choice('./video/hüpfen.mp4','hüpfen',true);
+var choiceWollknaeul = new Choice('./video/wollknäuel.mp4','Wollknäuel',false);
+var choiceWasser = new Choice('./video/wasser.mp4','Wasser',true);
+var choiceSchaf = new Choice('./video/schaf.mp4','Schaf',false);
+var choiceHuepfen = new Choice('./video/hüpfen.mp4','hüpfen',true);
+var choiceMaus = new Choice('./video/mouse.mp4','Maus',false);
+var choiceKrokodil = new Choice('./video/crocodile.mp4','Krokodil',false);
 
 
 
@@ -31,21 +33,52 @@ var choiceHuepfen= new Choice('./video/hüpfen.mp4','hüpfen',true);
 
 
 var frosch = new Animal('./img/frosch.png', './video/frosch.mp4', 'Frosch', [
-  choiceGreen,choiceWollknaeul,choiceWasser,choiceSchaf,choiceHuepfen
+  choiceGreen,choiceWollknaeul,choiceWasser,choiceSchaf,choiceHuepfen,choiceMaus,choiceKrokodil
 ]);
 var currentAnimal;
 var currentChoice;
 var currentBubbleElement;
+var bubbleMapping;
+var numSolved;
+
+var numBubbles = 5;
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
 
 function setPlayField(animal) {
+  numSolved = 0;
+
   currentAnimal = animal;
+  shuffleArray(currentAnimal.choices);
 
   animalImageElement.src = currentAnimal.animalPicture;
 }
 
 setPlayField(frosch);
 
+function initializeBubbles() {
+  // we will need this later on to swap choices in bubbles
+  bubbleMapping = [];
+  bubbleMapping[0] = 0;
+  bubbleMapping[1] = 1;
+  bubbleMapping[2] = 2;
+  bubbleMapping[3] = 3;
+  bubbleMapping[4] = 4;
 
+}
+
+initializeBubbles();
 
 function resetModal(){
   let choicesElement = document.getElementById('choices');
@@ -76,9 +109,14 @@ function question(torf) {
 
   if (currentChoice.correct === torf) {
     currentChoice.solved = true;
+    numSolved += 1;
     successElement.style.display = 'block'
     failElement.style.display = 'none'
     currentBubbleElement.classList.add('choiceBubbleSuccess');
+
+    if (numSolved == numBubbles) {
+      alert('Glückwunsch! Alle Blasen sind gelöst!')
+    }
   } else {
     failElement.style.display = 'block'
     successElement.style.display = 'none'
@@ -106,7 +144,9 @@ function choiceClick(bubbleId, bubbleElement) {
   let translationElement = document.getElementById('translation');
   let player = videojs('player');
 
-  currentChoice = currentAnimal.choices[bubbleId];
+  let choiceId = bubbleMapping[bubbleId];
+
+  currentChoice = currentAnimal.choices[choiceId];
   currentBubbleElement = bubbleElement;
 
   player.src(currentChoice.gesture);
@@ -123,3 +163,6 @@ function choiceClick(bubbleId, bubbleElement) {
     });
   }
 }
+
+
+
